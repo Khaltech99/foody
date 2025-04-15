013d11b4db1d74d2f9c3a3533a3b6258ea1e526f
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-15 09:02:38 +0100

    setup the store

--- useMealStore.js
+++ useMealStore.js
@@ -1,5 +1,20 @@
-// import { create } from "zustand";
+import { create } from "zustand";
 
-// export const mealStore = create((set) => ({
-//   categories: [],
-// }));
+export const useMealStore = create((set) => ({
+  categories: [],
+  loading: true,
+  fetchCategories: async () => {
+    try {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+      if (!response.ok) {
+        throw new Error("unable to fetch");
+      }
+      const data = await response.json();
+      set({ loading: false, categories: data.categories });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}));
