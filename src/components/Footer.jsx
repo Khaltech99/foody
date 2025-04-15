7c8cec0d8f55664b4af48ede024c5743db76b906
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-15 09:03:47 +0100

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Wrapper from "./Wrapper";
+import {
+  Facebook,
+  Instagram,
+  MessageCircle,
+  Search,
+  Twitter,
+} from "lucide-react";
+
+const Footer = () => {
+  return (
+    <div className="bg-black pb-10">
+      <Wrapper>
+        <section className="grid grid-cols-3 md:flex gap-4 md:flex-row md:justify-around py-10">
+          <div className="">
+            <h1 className="font-[Aclonica] text-2xl text-white md:text-3xl">
+              nageri
+            </h1>
+          </div>
+          <ul>
+            <li className="mb-4 text-white capitalize">Get started</li>
+            <li className="text-white text-sm">how to cook rice</li>
+          </ul>
+          <ul>
+            <li className="mb-4 text-white capitalize">Get started</li>
+            <li className="text-white text-sm">how to cook rice</li>
+          </ul>
+
+          <div className="flex flex-col">
+            <h1 className="text-white mb-4 capitalize">chat with us</h1>
+            <div className="flex gap-2 text-white">
+              <Twitter size={20} />
+              <Instagram size={20} />
+              <Facebook size={20} />
+            </div>
+          </div>
+        </section>
+        <div className="flex justify-center items-center gap-2 flex-1 bg-white rounded-lg w-full p-2  ">
+          <input
+            type="text"
+            placeholder="search any type of recipe you want"
+            className="text-sm outline-none bg-none w-full md:text-base"
+          />
+          <div className="bg-black w-fit p-3 text-white rounded-lg">
+            <Search />
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Footer;
