0b2e307516a5bcfc86396a779a737b08e9df3ce9
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-15 09:03:32 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import { Menu } from "lucide-react";
 import React from "react";
+import Wrapper from "./Wrapper";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center">
+    <nav className="flex justify-between items-center  bg-white w-full">
       <div>
         <h1 className="font-[Aclonica] text-3xl text-black md:text-4xl">
           nageri
         </h1>
       </div>
-      <Menu color="black" size={30} />
-    </div>
+      <button className="p-1 rounded-md hover:bg-gray-100">
+        <Menu color="black" size={30} />
+      </button>
+    </nav>
   );
 };
 
