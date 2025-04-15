0766379ddbdd325276c6e80ca84faf6d5f93cfc4
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-15 09:03:53 +0100

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import backgroundImage from "../assets/ingredients-near-pizza.jpg";
-import food from "../assets/407745033_05dbafde-b769-43ad-909e-a3ab5f6dd7ea.jpg";
+import food from "../assets/foodb-removebg-preview.png";
 import Button from "./Button";
 const Card = () => {
   return (
-    <div
-      className=" bg-cover bg-no-repeat w-full px-3 mt-4 rounded-lg flex justify-between"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="flex flex-col justify-around">
-        <p className=" text-base text-white capitalize">
+    <div className=" bg-[#222222] flex p-5 mt-4 rounded-lg justify-between items-center">
+      <div className="flex flex-col justify-between gap-3 md:gap-6">
+        <p className=" text-lg text-white capitalize md:text-5xl">
           discover the secrets to becoming a master chef
         </p>
-        <Button className="text-black font-bold bg-white text-sm w-fit">
+        <Button className="text-black justify-center items-center px-5 font-bold bg-white text-sm w-fit">
           start now
         </Button>
       </div>
-      <img src={food} alt="alt" className="w-32 h-32" />
+      <img src={food} alt="alt" className="w-auto h-28 md:w-auto md:h-80" />
     </div>
   );
 };
