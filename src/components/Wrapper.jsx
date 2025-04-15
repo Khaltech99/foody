ebc9a70408646cf28c01fd03b929479b14106347
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-15 09:03:41 +0100

    wrapper

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Wrapper = ({ children }) => {
   return (
-    <div className="m-3 font-[Poppins] md:w-[1200px] md:m-auto md:mt-5">
-      {children}
-    </div>
+    <div className="m-3 font-[Poppins] xl:w-7xl xl:mx-auto">{children}</div>
   );
 };
 
